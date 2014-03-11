43715b9110d48b1b6bbe61dda9ddd28bec6843a0
bauwen_j <bauwen_j@epitech.eu>
2014-03-11 15:38:31 +0000

    finish 108

--- 108monstre.class.rb
+++ 108monstre.class.rb
@@ -4,8 +4,36 @@ require_relative 'error/error.class.rb'
 
 class Monster
 
-  def initialize
-
+  def initialize a, val
+    @min = -2.0
+    @max = 2
+    
+    if val > 0
+      @val = val
+      @pas = val.abs / 1000
+    else
+      Error::call "error, argument is different of 0"
+    end
+    
+    if a >= 2
+      @a = a
+    else
+      Error::call "error, argument a = #{a} is invalide. superior at 2 only"
+    end
   end
 
+  def calc
+    tab = Array.new
+    while @min <= @max
+      k = 0.0
+      y = 0.0
+      while k < @val
+        y = y + (Math.cos(@a**k * Math::PI * @min) / 2**k)
+        k = k + 1
+      end
+      tab << y
+      @min = @min + 0.01
+    end
+    return tab
+  end
 end
--- 108monstre.rb
+++ 108monstre.rb
@@ -6,7 +6,29 @@ require_relative 'error/error.class.rb'
 require_relative '108monstre.class.rb'
 
 def main
+  s = ARGV.size
+  v = Gruff::Line.new ("1000x1000")
+  v.title = '108monstre'
 
+  if s != 2
+    Error::call "error : you need two argument"
+  end
+  if (Error::isnum? ARGV[0]) == false || (Error::isnum? ARGV[1]) == false
+    Error::call "error : number invalide"
+  end
+  g = Monster.new ARGV[0].to_f, ARGV[1].to_f
+  h = g.calc
+  
+
+  v.labels = { 0 => '-2', h.size / 8 => '-1.5',
+    h.size / 8 * 2 => '-1', h.size / 8 * 3 => '-0.5',
+    h.size / 2 => '0', h.size / 8 * 5 => '0.5',
+    h.size / 8 * 6 => '1', h.size / 8 * 7 => '1.5',
+    h.size - 1 => '2' }
+  v.data :"courbe monster", h
+  
+  v.write('108monstre.png')
+  `eog 108monstre.png`
 end
 
 main()
