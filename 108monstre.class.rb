4748d67e18fda5f843acfdb59ce18b751ba59daf
bauwen_j <bauwen_j@epitech.eu>
2014-03-11 11:14:15 +0000

    add

---
+++ 108monstre.class.rb
@@ -0,0 +1,11 @@
+#encoding: utf-8
+
+require_relative 'error/error.class.rb'
+
+class Monster
+
+  def initialize
+
+  end
+
+end
---
+++ 108monstre.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+#encoding: utf-8
+
+require 'gruff'
+require_relative 'error/error.class.rb'
+require_relative '108monstre.class.rb'
+
+def main
+
+end
+
+main()
